c3fb4bda30df25dbd6ca39318d5e054bfba4aa59
dragondgold <torti.max@gmail.com>
2014-06-13 00:23:40 -0300

    FilePicker for saving exported data!

--- src/main/java/com/andres/multiwork/pc/screens/ExportScreen.java
+++ src/main/java/com/andres/multiwork/pc/screens/ExportScreen.java
@@ -10,8 +10,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 public class ExportScreen extends MultiWorkScreen {
@@ -22,8 +24,9 @@ public class ExportScreen extends MultiWorkScreen {
     private TextArea description;
     private TextField title;
     private TextField fileName;
+    private Button filePickButton;
 
-    int channelToExport = -1;
+    private int channelToExport = -1;
 
     public ExportScreen(final Stage stage, final int width, final int height) {
         super(stage);
@@ -35,16 +38,18 @@ public class ExportScreen extends MultiWorkScreen {
 
             // Export button
             ((Button)fxmlLoader.getNamespace().get("exportButton")).setOnAction(event -> {
-                Exporter exporter = new Exporter(Decoder.getDecoder().getRawData(channelToExport), Decoder.getDecoder().getDecodedData(channelToExport));
-                exporter.setTitle(title.getText());
-                exporter.setDate(datePicker.getEditor().getText());
-                exporter.setDescription(description.getText());
-                exporter.setFileName(fileName.getText());
-                exporter.setExportData(exportDataCheck.isSelected());
-                exporter.setExportSamples(exportSamplesCheck.isSelected());
+                if(!fileName.getText().isEmpty()) {
+                    Exporter exporter = new Exporter(Decoder.getDecoder().getRawData(channelToExport), Decoder.getDecoder().getDecodedData(channelToExport));
+                    exporter.setTitle(title.getText());
+                    exporter.setDate(datePicker.getEditor().getText());
+                    exporter.setDescription(description.getText());
+                    exporter.setFileName(fileName.getText());
+                    exporter.setExportData(exportDataCheck.isSelected());
+                    exporter.setExportSamples(exportSamplesCheck.isSelected());
 
-                exporter.export();
-                getStage().hide();
+                    exporter.export();
+                    getStage().hide();
+                }
             });
 
             exportDataCheck = (CheckBox) fxmlLoader.getNamespace().get("dataExportCheck");
@@ -53,6 +58,17 @@ public class ExportScreen extends MultiWorkScreen {
             description = (TextArea) fxmlLoader.getNamespace().get("descriptionText");
             title = (TextField) fxmlLoader.getNamespace().get("titleText");
             fileName = (TextField) fxmlLoader.getNamespace().get("fileNameText");
+            filePickButton = (Button) fxmlLoader.getNamespace().get("filePickButton");
+
+            // Pick file store location
+            filePickButton.setOnAction(event -> {
+                FileChooser fileChooser = new FileChooser();
+                fileChooser.setTitle(GlobalValues.resourceBundle.getString("saveFile"));
+
+                // Retrieve location and file name
+                File file = fileChooser.showSaveDialog(getStage());
+                fileName.setText(file.getPath());
+            });
 
         } catch (IOException e) { e.printStackTrace(); }
     }
